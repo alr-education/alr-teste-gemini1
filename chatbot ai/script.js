a5a08d1c403d73065980d19cb87173e7d634f09c
alr-education <alr.educacao@gmail.com>
2025-05-02 16:43:43 -0300

    Update script.js

--- chatbot ai/script.js
+++ chatbot ai/script.js
@@ -1,4 +1,4 @@
-const API_KEY = 'API KEY'; 
+const API_KEY = 'AIzaSyBl0uf3quanoIYSbewkqxbj2HBe4cRat94'; 
 // Replace with your actual Gemini API key – this stores the API key to authenticate requests to the Gemini API.
 
 const API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent';
